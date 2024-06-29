4a328b2f26447ae4983a42065d9e0ad85f104246
jesus <jesus_estrada5@my.vcccd.edu>
2024-06-28 19:33:34 -0700

    CS V11

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 def main():
     result = []
+    
     while True:
-        start = input('Enter the starting letter: ')
-        end = input('Enter the starting letter: ')
+        start = input('Enter the starting letter: ').strip().lower()
+        end = input('Enter the starting letter: ').strip().lower()
 
         if len(start) != 1 or len(end) != 1 or not start.isalpha() or not end.isalpha():
             print("Error: Enter only alphabetic char")
             continue
         
-        if start <= end:
+        if start >= end:
             print("Error: Starting letter needs to be less than ending")
             continue
         
         letter = start
         while letter <= end:
             result.append(letter)
+            letter = chr(ord(letter) + 1)
+            
+        break
+    
+    print('Consecutive letters:', ' '.join(result))
         
-
-    print(*result)
 
     ########################################
     # Do not delete the return statement
